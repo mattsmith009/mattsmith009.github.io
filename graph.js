6ce7af51e2f95d5c02232461da1011421dda1cb4
mattsmith009 <msmith26@mit.edu>
2023-05-12 01:45:01 -0400

    a lot

--- graph.js
+++ graph.js
@@ -1,32 +1,3 @@
-/*
-const csv = require('csv-parser');
-const fs = require('fs');
-const results = [];
-const costPerCC = []
-const N = []
-
-// Use an array and for loop to get csv file data from all 12 files.
-fs.createReadStream('raw_data/cost_data_12709_m100.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-  for(let i = 0; i < results.length; i++) {
-      costPerCC.push(results[i].costPerCC);
-      N.push(results[i].N);
-  };
-
-  var nFloat = N.map(function(str) {
-    return parseFloat(str); });
-
-  var CPCCFloat = costPerCC.map(function(str) {
-    return parseFloat(str); });
-
-  console.log(nFloat, CPCCFloat)
-    }
-  )
-*/
-//import d3 from "d3-selection";
-
 // Open the navigation 
 function openNav() {
     document.getElementById("Sidenav").style.width = "250px";
@@ -40,6 +11,14 @@ function closeNav() {
   document.getElementById("Sidenav").style.width = "0";
   document.getElementById("myChart").style.width = "100%";
   }
+
+function isChecked(name_list) {
+  for (let i = 0; i < name_list.length; i++) {
+    if (document.getElemenentById(name_list[i]).checked) {
+      alert("checked");
+    }
+  }
+}
 
 
 
